49de925556e4e437585906b27d00e2e9dcb1b2ed
runArun <zl2613185@qq.com>
2017-11-01 11:26:42 +1100

    4 multiple components

---
+++ src/app/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <div className="navbar-header">
+                        <ul className="nav navbar-nav">
+                            <li><a href="">Home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
