18068150bab95ed0e2e9fe663b7bef47987d4e7c
runArun <zl2613185@qq.com>
2017-11-02 11:13:30 +1100

    6

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,18 +1,44 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
     render() {
 
-        let content = '';
+       /* let content = '';
         if(true) {
             content = <h1>hello, this is just a test !</h1>
-        }
+        }*/
+
+        console.log(this.props);
+
+        var text = 'something';
 
         return (
             <div>
                 <p>In a new Component!</p>
-                {content}
+                {/*{content}*/}
+                <p>{text}</p>
+                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+                <p>User Object => name: {this.props.user.name}</p>
+                <div className="">
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                      {/*  ES6 looping*/}
+                    </ul>
+                </div>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+
+/*
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    age : React.PropTypes.number,
+    user: React.PropTypes.object,
+    children: React.PropTypes.element.isRequired
+};*/
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,10 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render() {
+        var user = {
+            name :  "Anna",
+            hobbies : ['swimming','basketball']
+        }
         return (
             //document.createElement()
             <div className='container'>
@@ -16,7 +20,10 @@ class App extends React.Component {
                 </div>
                 <div className='row'>
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={'Max '} age={27} user={user}>
+                            <p>this is a wonderful paragraph</p>
+
+                        </Home>
                     </div>
                 </div>
             </div>
