bd41403aa2f8d335c0b127111aa53fbfc454bee7
runArun <zl2613185@qq.com>
2017-11-05 14:10:40 +1100

    router

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -10,8 +10,8 @@ export class User extends React.Component {
         return (
             <div>
                 <h3>The User Page</h3>
-                <p>User ID: {this.props.params.id}</p>
-                <button onClick={this.onNavigateHome} className="btn btn-primary">Go Home!</button>
+                <p>User ID: {this.props.match.params.param_name}</p>
+                <button onClick={this.onNavigateHome.bind(this)} className="btn btn-primary">Go Home!</button>
             </div>
         );
     }
