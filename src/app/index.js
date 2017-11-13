d614c995e0120c4896fe2d2ef059eb14dbfb0722
runArun <zl2613185@qq.com>
2017-11-13 11:19:35 +1100

    willMount didMount ...

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,30 @@
 import React from "react";
 import {render} from "react-dom";
-import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
-import {Root} from "./components/Root";
-import {Home} from "./components/Home";
-import {User} from "./components/User";
-
+import Clock from './Clock/Clock-component'
+import AutoFocusInput from './ref-Component'
 class App extends React.Component {
+
+    constructor () {
+        super()
+        this.state = { isShowClock: true }
+    }
+
+    handleShowOrHide () {
+        this.setState({
+            isShowClock: !this.state.isShowClock
+        })
+    }
+
     render() {
-        return (
-            <Router history={browserHistory}>
-                <Route path={"/"} component={Root} >
-                    <IndexRoute component={Home} />
-                    <Route path={"user/:id"} component={User} />
-                    <Route path={"home"} component={Home} />
-                </Route>
-                <Route path={"home-single"} component={Home}/>
-            </Router>
-        );
+        return(
+            <div>
+                {this.state.isShowClock ? <Clock /> : null }
+                <button onClick={this.handleShowOrHide.bind(this)}>显示或隐藏时钟</button>
+                <hr/>
+                <AutoFocusInput />
+            </div>
+        )
     }
 }
 
